54f512ffb25160baa794657992f3ca85fb753879
lololso <dcdc6811@gmail.com>
2024-04-11 14:02:58 +0800

    Create reuse star function

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,7 +1,5 @@
-// import { useState } from "react";
-
 import { useState } from "react";
-
+import PropTypes from "prop-types";
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -12,16 +10,31 @@ const starContainerStyle = {
   display: "flex",
 };
 
+StarRating.prototype = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTemprating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
@@ -32,7 +45,7 @@ export default function StarRating({
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -47,7 +60,11 @@ export default function StarRating({
           //   <span>S{i + 1}</span>
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
 
-import StartRating from "./StarRating";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={7} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5} />
-    <StartRating size={24} color="red" />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
